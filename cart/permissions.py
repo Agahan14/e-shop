e197d80f1a7e171f2f30f5e41517b62945e5d9b9
Agahan14 <Imamidinov.agahan06@gmail.com>
2022-03-14 00:40:49 +0600

    deleted

--- cart/permissions.py
+++ cart/permissions.py
@@ -1,9 +1,9 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
 
 
-class OwnerPermission(BasePermission):
+class OwnerPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
--- products/views.py
+++ products/views.py
@@ -7,30 +7,25 @@ from .serializers import (
     ProductSerializer
 )
 from .models import ProductCategory, Comment, Product
-from .permissions import IsSupplierPermission, OwnerPermission
 
 
 class ProductCategoryView(generics.ListCreateAPIView):
     serializer_class = ProductCategorySerializer
-    permission_classes = (IsSupplierPermission)
     queryset = ProductCategory.objects.all()
 
 
 class ProductCategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductCategorySerializer
-    permission_classes = (IsSupplierPermission)
     queryset = ProductCategory.objects.all()
 
 
 class ProductView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
-    permission_classes = (IsSupplierPermission)
     queryset = Product.objects.all()
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
-    permission_classes = (OwnerPermission)
     queryset = Product.objects.all()
 
 
