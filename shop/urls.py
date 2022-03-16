d8d5ae6666f2ff6c4769beb2962e8444a9123dff
Agahan14 <Imamidinov.agahan06@gmail.com>
2022-03-16 22:08:19 +0600

    fixed carts

--- shop/urls.py
+++ shop/urls.py
@@ -8,10 +8,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/", include("rest_framework.urls")),
     path('user/', include('users.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
-    path("login/", include("rest_framework.urls")),
     path("api/token/",
          TokenObtainPairView.as_view(),
          name="token_obtain_pair"
